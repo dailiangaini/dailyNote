22836e7fd18441c463d0c90158a39695cc3f6130
liangdai2 <liangdai2@chinaums.com>
2020-09-02 09:06:15 +0800

    项目初始化

--- src/main/java/dailiang/daily/note/daily.java
+++ src/main/java/dailiang/daily/note/daily.java
@@ -1,5 +1,6 @@
 package dailiang.daily.note;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,28 +10,33 @@ import java.util.Date;
  */
 public class daily {
     public static void main(String[] args) throws Exception {
-        int year = 2019;
-        int mouth = 10;
-        String baseDir = "/Users/dailiang/Desktop/";
-        String filePath = baseDir + mouth + "月份工作日志.md";
-        int days = Tools.getDayNumOfMouth(year, mouth);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int day = 1; day <= days ; day++) {
-            String dateString = Tools.getDateString(year, mouth, day);
-            Date date = simpleDateFormat.parse(dateString);
-            String weekOfDay = Tools.getWeekOfDate(date);
-            stringBuffer.append("###");
-            stringBuffer.append(" ");
-            stringBuffer.append(dateString);
-            stringBuffer.append(" ");
-            stringBuffer.append(weekOfDay);
-            stringBuffer.append("\n");
-            stringBuffer.append("```");
-            stringBuffer.append("\n");
-            stringBuffer.append("```");
-            stringBuffer.append("\n");
+        int year = 2022;
+        //int mouth = 10;
+        for (int mouth = 1; mouth <= 12; mouth++) {
+            String baseDir = "/Users/dailiang/Desktop/" + year + "/";
+            if(!new File(baseDir).exists()){
+                new File(baseDir).mkdirs();
+            }
+            String filePath = baseDir + mouth + "月份工作日志.md";
+            int days = Tools.getDayNumOfMouth(year, mouth);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            StringBuffer stringBuffer = new StringBuffer();
+            for (int day = 1; day <= days ; day++) {
+                String dateString = Tools.getDateString(year, mouth, day);
+                Date date = simpleDateFormat.parse(dateString);
+                String weekOfDay = Tools.getWeekOfDate(date);
+                stringBuffer.append("###");
+                stringBuffer.append(" ");
+                stringBuffer.append(dateString);
+                stringBuffer.append(" ");
+                stringBuffer.append(weekOfDay);
+                stringBuffer.append("\n");
+                stringBuffer.append("```");
+                stringBuffer.append("\n");
+                stringBuffer.append("```");
+                stringBuffer.append("\n");
+            }
+            Tools.writeString2File(filePath, stringBuffer.toString());
         }
-        Tools.writeString2File(filePath, stringBuffer.toString());
     }
 }
